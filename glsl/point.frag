c6c227788d8021a77ae2d3bae1e778741c6961bf
MrStronger <1274552194@qq.com>
2019-01-25 11:14:20 +0800

    [add]增加鼠标点击显示不同颜色功能

--- glsl/point.frag
+++ glsl/point.frag
@@ -1,4 +1,6 @@
 
+precision mediump float;
+uniform vec4 u_FragColor;
 void main() {
-  gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+  gl_FragColor = u_FragColor;
 }
--- js/main.js
+++ js/main.js
@@ -4,14 +4,16 @@ var gl = canvas.getContext('webgl')
 
 initShader(gl, 'glsl/point.vert', 'glsl/point.frag', (glProgram) => {
 
-  var a_Position = gl.getAttribLocation(glProgram, 'a_Position');
+  var a_Position = gl.getAttribLocation(glProgram, 'a_Position')
+  var u_FragColor = gl.getUniformLocation(glProgram, 'u_FragColor')
   gl.clearColor(1.0, 0.5, 0.5, 1.0)
   gl.clear(gl.COLOR_BUFFER_BIT)
   gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0)
+  gl.uniform4f(u_FragColor, ...[1.0,1.0,0.0,1.0])
   gl.drawArrays(gl.POINTS, 0, 1);
 
-  var points = [[0.0, 0.0]]
-  canvas.onclick = (ev) => onCanvasClick(ev, gl, canvas, a_Position, points)
+  var points = [[0.0, 0.0]], colors = [[1.0,1.0,0.0,1.0]]
+  canvas.onclick = (ev) => onCanvasClick(ev, gl, canvas, a_Position, u_FragColor, points, colors)
 })
 
 function initShader(gl, vsFile, fsFile, cb){
@@ -47,16 +49,18 @@ function initShader(gl, vsFile, fsFile, cb){
   var glProgram = gl.createProgram()
 }
 
-function onCanvasClick(ev, gl, canvas, a_Position, points) {
+function onCanvasClick(ev, gl, canvas, a_Position, u_FragColor, points, colors) {
   var x = ev.clientX, y = ev.clientY, canvasRect = canvas.getBoundingClientRect()
 
   x =(x - canvasRect.left - canvas.width/2) / (canvas.width/2)
   y = (canvas.height/2 - (y - canvasRect.top)) / (canvas.height/2)
   points.push([x,y])
+  colors.push([Math.abs(x),Math.abs(y),Math.abs(x+y-1),1.0])
   gl.clear(gl.COLOR_BUFFER_BIT)
 
   for(let i = 0; i < points.length; i++) {
     gl.vertexAttrib3f(a_Position, points[i][0], points[i][1], 0.0)
+    gl.uniform4f(u_FragColor, ...colors[i])
     gl.drawArrays(gl.POINTS, 0, 1);
   }
   
